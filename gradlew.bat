e84d299b972507e2c0c15f2f5ab4b3f583e03277
Kayuro <167020196+Kayuro@users.noreply.github.com>
2026-04-17 19:41:02 +0200

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+SET DIR=%~dp0
+IF "%DIR%"=="" SET DIR=.
+SET APP_BASE_NAME=%~n0
+SET APP_HOME=%DIR%
+SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+"%JAVA_HOME%\bin\java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
